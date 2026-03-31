2b985b84243e00a1f283d0910f3dfa847e050492
yustafia <devalovegood@gmail.com>
2026-03-31 13:29:42 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -117,7 +117,9 @@ class Snake(GameObject):
             pygame.draw.rect(screen, self.body_color, rect)
             pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
-        head_rect = pygame.Rect(self.get_head_position(), (GRID_SIZE, GRID_SIZE))
+        head_rect = pygame.Rect(
+            self.get_head_position(), (GRID_SIZE, GRID_SIZE)
+        )
         pygame.draw.rect(screen, self.body_color, head_rect)
         pygame.draw.rect(screen, BORDER_COLOR, head_rect, 1)
 
